6aadb6fba94b5c3bb7b930ac73869470eee97bca
RayMarch <RayMarch@users.noreply.github.com>
2022-09-08 20:27:29 +0200

    added control flow example module

--- examples/api_showcase/src/main.rs
+++ examples/api_showcase/src/main.rs
@@ -5,12 +5,13 @@ mod simple_render_pipeline;
 mod structs_example;
 mod vec_matrix_example;
 mod functions_example;
+mod control_flow;
 mod mirror_example;
 mod compute_pipeline;
 mod render_pipeline;
 mod known_surprising_behavior;
 
 fn main() {
-    //readme_example::main();
-    known_surprising_behavior::main();
+    control_flow::main();
+    //known_surprising_behavior::main();
 }
