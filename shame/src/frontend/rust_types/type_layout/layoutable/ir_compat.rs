1aecb7e3725535c4b2d9af3bd628756377ffe715
chronicl <lennard.koehler@gmx.net>
2025-05-24 02:57:09 +0200

    Make ContainsPackedVecError simple

--- shame/src/frontend/rust_types/type_layout/layoutable/ir_compat.rs
+++ shame/src/frontend/rust_types/type_layout/layoutable/ir_compat.rs
@@ -6,107 +6,14 @@ use super::*;
 #[derive(thiserror::Error, Debug)]
 pub enum IRConversionError {
     /// Packed vectors do not exist in the shader type system.
-    #[error("{0}")]
-    ContainsPackedVector(#[from] ContainsPackedVectorError),
+    #[error(
+        "Type is or contains a packed vector, which does not exist in the shader type system.\n\
+        Packed vectors may only be used in vertex buffers."
+    )]
+    ContainsPackedVector,
     /// Struct field names must be unique in the shader type system.
     #[error("{0}")]
     DuplicateFieldName(#[from] DuplicateFieldNameError),
-}
-
-#[derive(Debug)]
-pub enum ContainsPackedVectorError {
-    /// Top level type is a packed vector.
-    SelfIsPackedVector(LayoutableType),
-    /// Top level nested Array packed vector, for example Array<Array<Array<unorm8x2>>>.
-    InArray(LayoutableType),
-    /// A struct field is a packed vector or a struct field is a nested Array packed vector.
-    InStruct {
-        struct_type: StructKind,
-        packed_vector_field: usize,
-        top_level_type: Option<LayoutableType>,
-        use_color: bool,
-    },
-}
-
-impl std::error::Error for ContainsPackedVectorError {}
-
-impl std::fmt::Display for ContainsPackedVectorError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let ending =
-            ", which does not exist in the shader type system.\nPacked vectors may only be used in vertex buffers.";
-        let (struct_name, sized_fields, last_unsized, packed_vector_field, use_color) = match self {
-            ContainsPackedVectorError::SelfIsPackedVector(t) => return writeln!(f, "{t} is a packed vector{ending}"),
-            ContainsPackedVectorError::InArray(a) => return writeln!(f, "{a} contains a packed vector{ending}"),
-            ContainsPackedVectorError::InStruct {
-                struct_type,
-                packed_vector_field,
-                top_level_type,
-                use_color,
-            } => {
-                let (struct_name, sized_fields, last_unsized) = match struct_type {
-                    StructKind::Sized(s) => (&s.name, s.fields(), None),
-                    StructKind::Unsized(s) => (&s.name, s.sized_fields.as_slice(), Some(&s.last_unsized)),
-                };
-
-                if let Some(top_level_type) = top_level_type {
-                    writeln!(
-                        f,
-                        "The struct {struct_name} in {top_level_type} contains a packed vector{ending}"
-                    )?;
-                } else {
-                    writeln!(f, "{struct_name} contains a packed vector{ending}")?;
-                }
-
-                (
-                    struct_name,
-                    sized_fields,
-                    last_unsized,
-                    *packed_vector_field,
-                    *use_color,
-                )
-            }
-        };
-
-
-
-        let indent = "  ";
-        let is_packed_vec = |i| packed_vector_field == i;
-        let arrow = |i| match is_packed_vec(i) {
-            true => " <--",
-            false => "",
-        };
-        let color = |f: &mut Formatter<'_>, i| {
-            if (use_color && is_packed_vec(i)) {
-                set_color(f, Some("#508EE3"), false)
-            } else {
-                Ok(())
-            }
-        };
-        let color_reset = |f: &mut Formatter<'_>, i| {
-            if use_color && is_packed_vec(i) {
-                set_color(f, None, false)
-            } else {
-                Ok(())
-            }
-        };
-
-        writeln!(f, "The following struct contains a packed vector:")?;
-        let header = writeln!(f, "struct {} {{", struct_name);
-        for (i, field) in sized_fields.iter().enumerate() {
-            color(f, i)?;
-            writeln!(f, "{indent}{}: {},{}", field.name, field.ty, arrow(i))?;
-            color_reset(f, i)?;
-        }
-        if let Some(field) = last_unsized {
-            let i = sized_fields.len();
-            color(f, i)?;
-            writeln!(f, "{indent}{}: {},{}", field.name, field.array, arrow(i))?;
-            color_reset(f, i)?;
-        }
-        writeln!(f, "}}")?;
-
-        Ok(())
-    }
 }
 
 #[derive(Debug)]
@@ -114,7 +21,6 @@ pub struct DuplicateFieldNameError {
     pub struct_type: StructKind,
     pub first_field: usize,
     pub second_field: usize,
-    pub is_top_level: bool,
     pub use_color: bool,
 }
 
@@ -173,14 +79,9 @@ impl std::fmt::Display for DuplicateFieldNameError {
 }
 
 fn check_for_duplicate_field_names(
-    struct_type: StructKind,
-    is_top_level: bool,
-) -> Result<StructKind, DuplicateFieldNameError> {
-    let (sized_fields, last_unsized) = match &struct_type {
-        StructKind::Sized(s) => (s.fields(), None),
-        StructKind::Unsized(s) => (s.sized_fields.as_slice(), Some(&s.last_unsized)),
-    };
-
+    sized_fields: &[SizedField],
+    last_unsized: Option<&RuntimeSizedArrayField>,
+) -> Option<(usize, usize)> {
     // Brute force search > HashMap for the amount of fields
     // we'd usually deal with.
     let mut duplicate_fields = None;
@@ -191,18 +92,14 @@ fn check_for_duplicate_field_names(
                 break;
             }
         }
+        if let Some(last_unsized) = last_unsized {
+            if field1.name == last_unsized.name {
+                duplicate_fields = Some((i, sized_fields.len()));
+                break;
+            }
+        }
     }
-
-    match duplicate_fields {
-        Some((first_field, second_field)) => Err(DuplicateFieldNameError {
-            struct_type,
-            first_field,
-            second_field,
-            is_top_level,
-            use_color: use_color(),
-        }),
-        None => Ok(struct_type),
-    }
+    duplicate_fields
 }
 
 #[track_caller]
@@ -212,62 +109,32 @@ impl TryFrom<LayoutableType> for ir::StoreType {
     type Error = IRConversionError;
 
     fn try_from(ty: LayoutableType) -> Result<Self, Self::Error> {
-        // Checking for top level packed vec.
-        if matches!(ty, LayoutableType::Sized(SizedType::PackedVec(_))) {
-            return Err(IRConversionError::ContainsPackedVector(
-                ContainsPackedVectorError::SelfIsPackedVector(ty),
-            ));
-        }
-        // Checking for nested array packed vec.
-        if is_packed_vec(&ty) {
-            return Err(IRConversionError::ContainsPackedVector(
-                ContainsPackedVectorError::InArray(ty),
-            ));
-        }
-
-        fn convert
-
-        todo!()
-    }
-}
-
-/// Returns true if `ty` is a packed vector or a nesting of Arrays
-/// with the last Array directly containing a packed vector.
-fn is_packed_vec(ty: &LayoutableType) -> bool {
-    let mut ty = match ty {
-        LayoutableType::Sized(s) => s,
-        LayoutableType::RuntimeSizedArray(a) => &a.element,
-        LayoutableType::UnsizedStruct(_) => return false,
-    };
-
-    loop {
         match ty {
-            SizedType::PackedVec(_) => return true,
-            SizedType::Array(a) => ty = &a.element,
-            _ => return false,
+            LayoutableType::Sized(s) => Ok(ir::StoreType::Sized(s.try_into()?)),
+            LayoutableType::RuntimeSizedArray(s) => Ok(ir::StoreType::RuntimeSizedArray(s.element.try_into()?)),
+            LayoutableType::UnsizedStruct(s) => Ok(ir::StoreType::BufferBlock(s.try_into()?)),
         }
     }
 }
 
-// impl TryFrom<SizedType> for ir::SizedType {
-//     type Error = IRConversionError;
+impl TryFrom<SizedType> for ir::SizedType {
+    type Error = IRConversionError;
 
-//     fn try_from(host: SizedType) -> Result<Self, Self::Error> {
-//         // Ok(match host {
-//         //     SizedType::Vector(v) => ir::SizedType::Vector(v.len, v.scalar.into()),
-//         //     SizedType::Matrix(m) => ir::SizedType::Matrix(m.columns, m.rows, m.scalar),
-//         //     SizedType::Array(a) => {
-//         //         let element = Rc::unwrap_or_clone(a.element);
-//         //         let converted_element = element.try_into()?;
-//         //         ir::SizedType::Array(Rc::new(converted_element), a.len)
-//         //     }
-//         //     SizedType::Atomic(i) => ir::SizedType::Atomic(i.scalar),
-//         //     SizedType::PackedVec(_) => return Err(IRConversionError::ContainsPackedVector),
-//         //     SizedType::Struct(s) => ir::SizedType::Structure(s.try_into()?),
-//         // })
-//         todo!()
-//     }
-// }
+    fn try_from(host: SizedType) -> Result<Self, Self::Error> {
+        Ok(match host {
+            SizedType::Vector(v) => ir::SizedType::Vector(v.len, v.scalar.into()),
+            SizedType::Matrix(m) => ir::SizedType::Matrix(m.columns, m.rows, m.scalar),
+            SizedType::Array(a) => {
+                let element = Rc::unwrap_or_clone(a.element);
+                let converted_element = element.try_into()?;
+                ir::SizedType::Array(Rc::new(converted_element), a.len)
+            }
+            SizedType::Atomic(i) => ir::SizedType::Atomic(i.scalar),
+            SizedType::PackedVec(_) => return Err(IRConversionError::ContainsPackedVector),
+            SizedType::Struct(s) => ir::SizedType::Structure(s.try_into()?),
+        })
+    }
+}
 
 impl From<ScalarType> for ir::ScalarType {
     fn from(scalar_type: ScalarType) -> Self {
@@ -281,86 +148,95 @@ impl From<ScalarType> for ir::ScalarType {
     }
 }
 
-// impl TryFrom<SizedStruct> for ir::ir_type::SizedStruct {
-//     type Error = IRConversionError;
+impl TryFrom<SizedStruct> for ir::ir_type::SizedStruct {
+    type Error = IRConversionError;
 
-//     fn try_from(host: SizedStruct) -> Result<Self, Self::Error> {
-//         let mut fields: Vec<ir::ir_type::SizedField> = Vec::new();
+    fn try_from(ty: SizedStruct) -> Result<Self, Self::Error> {
+        if let Some((first, second)) = check_for_duplicate_field_names(ty.fields(), None) {
+            return Err(IRConversionError::DuplicateFieldName(DuplicateFieldNameError {
+                struct_type: StructKind::Sized(ty),
+                first_field: first,
+                second_field: second,
+                use_color: use_color(),
+            }));
+        }
 
-//         for field in host.fields {
-//             fields.push(field.try_into()?);
-//         }
+        let mut fields: Vec<ir::ir_type::SizedField> = Vec::new();
+        for field in ty.fields {
+            fields.push(field.try_into()?);
+        }
+        let last_field = fields.pop().unwrap();
 
-//         // has at least one field
-//         if fields.is_empty() {
-//             // This should not happen based on SizedStruct's implementation
-//             return Err(IRConversionError::DuplicateFieldName);
-//         }
+        match ir::ir_type::SizedStruct::new_nonempty(ty.name, fields, last_field) {
+            Ok(s) => Ok(s),
+            Err(StructureFieldNamesMustBeUnique) => unreachable!("checked above"),
+        }
+    }
+}
 
-//         let last_field = fields.pop().unwrap();
+impl TryFrom<UnsizedStruct> for ir::ir_type::BufferBlock {
+    type Error = IRConversionError;
 
-//         match ir::ir_type::SizedStruct::new_nonempty(host.name, fields, last_field) {
-//             Ok(s) => Ok(s),
-//             Err(StructureFieldNamesMustBeUnique) => Err(IRConversionError::DuplicateFieldName),
-//         }
-//     }
-// }
+    fn try_from(ty: UnsizedStruct) -> Result<Self, Self::Error> {
+        if let Some((first, second)) = check_for_duplicate_field_names(&ty.sized_fields, Some(&ty.last_unsized)) {
+            return Err(IRConversionError::DuplicateFieldName(DuplicateFieldNameError {
+                struct_type: StructKind::Unsized(ty),
+                first_field: first,
+                second_field: second,
+                use_color: use_color(),
+            }));
+        }
 
-// impl TryFrom<UnsizedStruct> for ir::ir_type::BufferBlock {
-//     type Error = IRConversionError;
+        let mut sized_fields: Vec<ir::ir_type::SizedField> = Vec::new();
 
-//     fn try_from(host: UnsizedStruct) -> Result<Self, Self::Error> {
-//         let mut sized_fields: Vec<ir::ir_type::SizedField> = Vec::new();
+        for field in ty.sized_fields {
+            sized_fields.push(field.try_into()?);
+        }
 
-//         for field in host.sized_fields {
-//             sized_fields.push(field.try_into()?);
-//         }
+        let last_unsized = ty.last_unsized.try_into()?;
 
-//         let last_unsized = host.last_unsized.try_into()?;
+        match ir::ir_type::BufferBlock::new(ty.name, sized_fields, Some(last_unsized)) {
+            Ok(b) => Ok(b),
+            Err(BufferBlockDefinitionError::FieldNamesMustBeUnique) => unreachable!("checked above"),
+            Err(BufferBlockDefinitionError::MustHaveAtLeastOneField) => {
+                unreachable!("last_unsized is at least one field")
+            }
+        }
+    }
+}
 
-//         match ir::ir_type::BufferBlock::new(host.name, sized_fields, Some(last_unsized)) {
-//             Ok(b) => Ok(b),
-//             Err(BufferBlockDefinitionError::FieldNamesMustBeUnique) => Err(IRConversionError::DuplicateFieldName),
-//             Err(BufferBlockDefinitionError::MustHaveAtLeastOneField) => {
-//                 // This should not happen based on UnsizedStruct's implementation
-//                 Err(IRConversionError::DuplicateFieldName)
-//             }
-//         }
-//     }
-// }
+impl TryFrom<RuntimeSizedArray> for ir::StoreType {
+    type Error = IRConversionError;
 
-// impl TryFrom<RuntimeSizedArray> for ir::StoreType {
-//     type Error = IRConversionError;
+    fn try_from(array: RuntimeSizedArray) -> Result<Self, Self::Error> {
+        Ok(ir::StoreType::RuntimeSizedArray(array.element.try_into()?))
+    }
+}
 
-//     fn try_from(array: RuntimeSizedArray) -> Result<Self, Self::Error> {
-//         Ok(ir::StoreType::RuntimeSizedArray(array.element.try_into()?))
-//     }
-// }
+impl TryFrom<SizedField> for ir::ir_type::SizedField {
+    type Error = IRConversionError;
 
-// impl TryFrom<SizedField> for ir::ir_type::SizedField {
-//     type Error = IRConversionError;
+    fn try_from(f: SizedField) -> Result<Self, Self::Error> {
+        Ok(ir::SizedField::new(
+            f.name,
+            f.custom_min_size,
+            f.custom_min_align,
+            f.ty.try_into()?,
+        ))
+    }
+}
 
-//     fn try_from(f: SizedField) -> Result<Self, Self::Error> {
-//         Ok(ir::SizedField::new(
-//             f.name,
-//             f.custom_min_size,
-//             f.custom_min_align,
-//             f.ty.try_into()?,
-//         ))
-//     }
-// }
+impl TryFrom<RuntimeSizedArrayField> for ir::ir_type::RuntimeSizedArrayField {
+    type Error = IRConversionError;
 
-// impl TryFrom<RuntimeSizedArrayField> for ir::ir_type::RuntimeSizedArrayField {
-//     type Error = IRConversionError;
-
-//     fn try_from(f: RuntimeSizedArrayField) -> Result<Self, Self::Error> {
-//         Ok(ir::RuntimeSizedArrayField::new(
-//             f.name,
-//             f.custom_min_align,
-//             f.array.element.try_into()?,
-//         ))
-//     }
-// }
+    fn try_from(f: RuntimeSizedArrayField) -> Result<Self, Self::Error> {
+        Ok(ir::RuntimeSizedArrayField::new(
+            f.name,
+            f.custom_min_align,
+            f.array.element.try_into()?,
+        ))
+    }
+}
 
 
 //     Conversions from ir types     //
